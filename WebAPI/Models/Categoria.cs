5e1b0130962164e86602ae6aa740f7a7ed6fd5fb
Dani Santos <dani_cs01@hotmail.com>
2019-09-04 15:53:12 -0300

    Add Commit Inicial + Add Entidades Categoria e Produto

---
+++ WebAPI/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
---
+++ WebAPI/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public string Imagem { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public DateTime UltimaAtualizacao { get; set; }
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+    }
+}
